80e14063a57fbbfa4b9b02dda0b0b2af3b8cdc3b
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-16 05:08:11 -0400

    adds LoginUserCommandHandler and LoginResponseDTO

---
+++ projects/services/users/application/src/lib/commands/handlers/LoginUserCommandHandler.ts
@@ -0,0 +1,27 @@
+import { IUserAuthenticator, IUsersDomainRulesConfigProvider, Password, UserName } from "users-domain";
+import { ICommandHandler, ICommandHandlerAsync, ICommandValidator } from "common";
+import { LoginUserCommand } from "../LoginUserCommand";
+import { LoginResponseDTO } from "../../dtos/LoginResponseDTO";
+
+export class LoginUserCommandHandler implements ICommandHandlerAsync<LoginUserCommand,LoginResponseDTO>{
+    
+    constructor(
+        private readonly validator:ICommandValidator<LoginUserCommand>,
+        private readonly userAuthenticator:IUserAuthenticator,
+        private readonly configProvider:IUsersDomainRulesConfigProvider
+    ){
+
+    }
+    async handle(command: LoginUserCommand): Promise<LoginResponseDTO> {
+        try {
+            this.validator.validate(command);
+            const username = new UserName(command.data.username,this.configProvider.UserNameDomaiRules);
+            const password = new Password(command.data.password,this.configProvider.PasswordDomainRules);
+            const logged = await this.userAuthenticator.authenticate(username,password);
+            return { logged } 
+        } catch (error) {
+            throw error;
+        }
+    }
+
+}
