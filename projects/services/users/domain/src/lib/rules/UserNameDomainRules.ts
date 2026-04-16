73e0fa2bbd95cb68b208587396bcecb9567aa87c
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-16 01:26:00 -0400

    adds DomainRules

---
+++ projects/services/users/domain/src/lib/rules/UserNameDomainRules.ts
@@ -0,0 +1,33 @@
+import { IDomainRule } from "common";
+import { InvalidUserNameError } from "../errors/InvalidUserNameError";
+
+export class UserNameDomainRules implements IDomainRule<string> {
+    constructor(
+        readonly minLength: number,
+        readonly maxLenght: number
+    ) {
+
+    }
+    isValid(value: string): boolean {
+        try {
+            this.check(value);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    check(value: string): void {
+        if (!value || value.trim().length === 0) {
+            throw new InvalidUserNameError("value can't be null nor whitespace");
+        }
+        if (value.length < this.minLength || value.length > this.maxLenght) {
+            throw new InvalidUserNameError(`value length must be between ${this.minLength} and ${this.maxLenght}`);
+        }
+        const pattern = /^[a-zA-Z0-9_]$/;
+        if (!pattern.test(value)) {
+            throw new InvalidUserNameError("value must only contains alphanumeric and underscore characters");
+        }
+    }
+
+}
