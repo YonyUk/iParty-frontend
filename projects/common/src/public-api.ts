c34563700e220839115c7dc4da586fd3dd04ffc1
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-16 00:32:13 -0400

    adds Errors definitions

---
+++ projects/common/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of common
+ */
+
+export * from './lib/errors/InvalidValueError';
+export * from './lib/errors/AlreadyExistsError';
+export * from './lib/errors/NotFoundError';
+export * from './lib/errors/RequiredError';
