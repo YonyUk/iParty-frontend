f4ee0733851cb37c4f22c5487e1de225ac0d7cf5
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-16 05:32:39 -0400

    builds users-application project

--- projects/services/users/application/src/public-api.ts
+++ projects/services/users/application/src/public-api.ts
@@ -2,3 +2,13 @@
  * Public API Surface of users-application
  */
 
+export * from './lib/commands/LoginUserCommand';
+export * from './lib/commands/RegisterUserCommand';
+export * from './lib/commands/handlers/LoginUserCommandHandler';
+export * from './lib/commands/handlers/RegisterUserCommandHandler';
+export * from './lib/commands/validators/LoginUserCommandValidator';
+export * from './lib/commands/validators/RegisterUserCommandValidator';
+export * from './lib/dtos/LoginCredentialsDTO';
+export * from './lib/dtos/LoginResponseDTO';
+export * from './lib/dtos/RegisterUserDTO';
+export * from './lib/dtos/RegisterUserResponseDTO';
