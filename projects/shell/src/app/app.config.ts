72b5cb91e5e364e943d486037672f530f39097c7
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-18 06:14:34 -0400

    adds apiHost token provider

--- projects/shell/src/app/app.config.ts
+++ projects/shell/src/app/app.config.ts
@@ -5,12 +5,18 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 import { provideUsersInfrastructure } from 'users-infrastructure';
+import { API_HOST_TOKEN } from 'common';
+import { environment } from '../environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient(),
-    provideUsersInfrastructure()
-  ],
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(routes),
+		provideHttpClient(),
+		provideUsersInfrastructure(),
+		{
+			provide: API_HOST_TOKEN,
+			useValue: environment.apiHost
+		}
+	],
 };
