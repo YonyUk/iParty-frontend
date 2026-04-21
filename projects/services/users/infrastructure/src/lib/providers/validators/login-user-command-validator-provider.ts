9b02058ac9b7636ad5e42bf27bc14276b53b45ab
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-21 04:49:14 -0400

    fix: LOGIN_USER_COMMAND_VALIDATOR_TOKEN provider

--- projects/services/users/infrastructure/src/lib/providers/validators/login-user-command-validator-provider.ts
+++ projects/services/users/infrastructure/src/lib/providers/validators/login-user-command-validator-provider.ts
@@ -1,10 +1,10 @@
 import { inject, Provider } from "@angular/core";
 import { LoginUserCommandValidator } from "users-application";
-import { USERS_DOMAIN_RULES_CONFIG_PROVIDER_TOKEN } from "../../tokens";
+import { LOGIN_USER_COMMAND_VALIDATOR_TOKEN, USERS_DOMAIN_RULES_CONFIG_PROVIDER_TOKEN } from "../../tokens";
 
 export function provideLoginUserCommandValidator():Provider{
   return {
-    provide:LoginUserCommandValidator,
+    provide:LOGIN_USER_COMMAND_VALIDATOR_TOKEN,
     useFactory:() => {
       const config = inject(USERS_DOMAIN_RULES_CONFIG_PROVIDER_TOKEN);
       return new LoginUserCommandValidator(config)
